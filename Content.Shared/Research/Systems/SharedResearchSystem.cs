781d7cba6c33eb78b5cddd57eb3848e2c8f919dc
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2024-09-20 10:14:12 +0300

    [Port] RND Research Rework (#750)
    * [Port] RND Research Rework
    
    * up1
    
    * backmen research ftl
    
    * up2
    
    * fuck
    
    * zaebal cannot be unlocked on any lathes

--- Content.Shared/Research/Systems/SharedResearchSystem.cs
+++ Content.Shared/Research/Systems/SharedResearchSystem.cs
@@ -150,7 +150,7 @@ public abstract class SharedResearchSystem : EntitySystem
         TechnologyPrototype technology,
         bool includeCost = true,
         bool includeTier = true,
-        bool includePrereqs = false,
+        bool includePrereqs = true, # Corvax-Next-EDIT-RNDResearch
         TechDisciplinePrototype? disciplinePrototype = null)
     {
         var description = new FormattedMessage();
