888f77caf60c6c2e56d9c61767c414e4f168a4f3
PuroSlavKing <puroslavking@yahoo.com>
2024-11-15 22:59:35 +0300

    [Port] Autovote System
    Co-Authored-By: sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>

--- Content.Shared/_CorvaxNext/NextVars.cs
+++ Content.Shared/_CorvaxNext/NextVars.cs
@@ -9,9 +9,33 @@ namespace Content.Shared._CorvaxNext.NextVars;
 // ReSharper disable once InconsistentNaming
 public sealed class NextVars
 {
-    /// <summary>
-    /// Offer item.
-    /// </summary>
-    public static readonly CVarDef<bool> OfferModeIndicatorsPointShow =
-        CVarDef.Create("hud.offer_mode_indicators_point_show", true, CVar.ARCHIVE | CVar.CLIENTONLY);
+        /// <summary>
+        /// Offer item.
+        /// </summary>
+        public static readonly CVarDef<bool> OfferModeIndicatorsPointShow =
+            CVarDef.Create("hud.offer_mode_indicators_point_show", true, CVar.ARCHIVE | CVar.CLIENTONLY);
+
+        /*
+        * AUTOVOTE SYSTEM
+        */
+
+        /// <summary>
+        /// Enables the automatic voting system.
+        /// <summary>
+        public static readonly CVarDef<bool> AutoVoteEnabled =
+            CVarDef.Create("vote.autovote_enabled", false, CVar.SERVERONLY);
+
+        /// <summary>
+        /// Automatically starts a map vote when returning to the lobby.
+        /// Requires auto voting to be enabled.
+        /// <summary>
+        public static readonly CVarDef<bool> MapAutoVoteEnabled =
+            CVarDef.Create("vote.map_autovote_enabled", true, CVar.SERVERONLY);
+
+        /// <summary>
+        /// Automatically starts a gamemode vote when returning to the lobby.
+        /// Requires auto voting to be enabled.
+        /// <summary>
+        public static readonly CVarDef<bool> PresetAutoVoteEnabled =
+            CVarDef.Create("vote.preset_autovote_enabled", true, CVar.SERVERONLY);
 }
