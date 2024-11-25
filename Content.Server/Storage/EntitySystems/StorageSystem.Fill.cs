ca96f2b5470ce6adc0645c4402cdbb0d57a55bda
Vonsant <von7ant@gmail.com>
2024-11-25 23:12:06 +0300

    SomeFixes2

--- Content.Server/Storage/EntitySystems/StorageSystem.Fill.cs
+++ Content.Server/Storage/EntitySystems/StorageSystem.Fill.cs
@@ -31,7 +31,7 @@ public sealed partial class StorageSystem
         }
     }
 
-    private void FillStorage(Entity<StorageFillComponent?, StorageComponent?> entity)
+    public void FillStorage(Entity<StorageFillComponent?, StorageComponent?> entity) // Corvax NEXT-cards
     {
         var (uid, component, storage) = entity;
 
