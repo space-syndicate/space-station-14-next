a87d1225e0bbab2aefdfb6b73254dd46aa351ab3
Vonsant <von7ant@gmail.com>
2024-12-03 19:04:08 +0300

    Fixes

--- Content.Server/_CorvaxNext/Storage/AnchorableStorageSystem.cs
+++ Content.Server/_CorvaxNext/Storage/AnchorableStorageSystem.cs
@@ -11,8 +11,8 @@ using Robust.Shared.Map.Components;
 namespace Content.Server._CorvaxNext.Storage;
 
 /// <summary>
-/// This is used for restricting anchor operations on storage (one bag max per tile)
-/// and ejecting living contents on anchor.
+/// Используется для ограничения операций якорения хранилищ (не более одной сумки на тайл)
+/// и выброса живых содержимых при якорении.
 /// </summary>
 public sealed class AnchorableStorageSystem : EntitySystem
 {
@@ -29,81 +29,84 @@ public sealed class AnchorableStorageSystem : EntitySystem
         SubscribeLocalEvent<AnchorableStorageComponent, ContainerIsInsertingAttemptEvent>(OnInsertAttempt);
     }
 
-    private void OnAnchorStateChanged(Entity<AnchorableStorageComponent> ent, ref AnchorStateChangedEvent args)
+    private void OnAnchorStateChanged(EntityUid uid, AnchorableStorageComponent comp, ref AnchorStateChangedEvent args)
     {
         if (!args.Anchored)
             return;
 
-        if (CheckOverlap((ent, ent.Comp, Transform(ent))))
+        if (!TryComp<TransformComponent>(uid, out var xform))
+            return;
+
+        if (CheckOverlap(uid))
         {
-            _popup.PopupEntity(Loc.GetString("anchored-storage-already-present"), ent);
-            _xform.Unanchor(ent, Transform(ent));
+            _popup.PopupEntity(Loc.GetString("anchored-storage-already-present"), uid);
+            _xform.Unanchor(uid, xform);
             return;
         }
 
-        // Eject any sapient creatures inside the storage.
-        // Does not recurse down into bags in bags - player characters are the largest concern, and they'll only fit in duffelbags.
-        if (!TryComp(ent.Owner, out StorageComponent? storage))
+        // Выбрасываем любые разумные существа внутри хранилища.
+        if (!TryComp<StorageComponent>(uid, out var storage))
             return;
 
         var entsToRemove = storage.StoredItems.Keys.Where(storedItem =>
                 HasComp<MindContainerComponent>(storedItem)
-            ).ToList();
+            ).ToArray();
 
         foreach (var removeUid in entsToRemove)
-            _container.RemoveEntity(ent.Owner, removeUid);
+            _container.RemoveEntity(uid, removeUid);
     }
 
-    private void OnAnchorAttempt(Entity<AnchorableStorageComponent> ent, ref AnchorAttemptEvent args)
+    private void OnAnchorAttempt(EntityUid uid, AnchorableStorageComponent comp, ref AnchorAttemptEvent args)
     {
         if (args.Cancelled)
             return;
 
-        // Nothing around? We can anchor without issue.
-        if (!CheckOverlap((ent, ent.Comp, Transform(ent))))
+        if (!TryComp<TransformComponent>(uid, out var xform))
             return;
 
-        _popup.PopupEntity(Loc.GetString("anchored-storage-already-present"), ent, args.User);
+        // Если вокруг ничего нет, можем якориться без проблем.
+        if (!CheckOverlap(uid))
+            return;
+
+        _popup.PopupEntity(Loc.GetString("anchored-storage-already-present"), uid, args.User);
         args.Cancel();
     }
 
-    private void OnInsertAttempt(Entity<AnchorableStorageComponent> ent, ref ContainerIsInsertingAttemptEvent args)
+    private void OnInsertAttempt(EntityUid uid, AnchorableStorageComponent comp, ref ContainerIsInsertingAttemptEvent args)
     {
         if (args.Cancelled)
             return;
 
-        // Check for living things, they should not insert when anchored.
+        // Проверяем на наличие живых существ, они не должны вставляться при якорении.
         if (!HasComp<MindContainerComponent>(args.EntityUid))
             return;
 
-        if (Transform(ent.Owner).Anchored)
+        if (!TryComp<TransformComponent>(uid, out var xform))
+            return;
+
+        if (xform.Anchored)
             args.Cancel();
     }
 
     [PublicAPI]
     public bool CheckOverlap(EntityUid uid)
     {
-        if (!TryComp(uid, out AnchorableStorageComponent? comp))
+        if (!TryComp<TransformComponent>(uid, out var xform))
             return false;
 
-        return CheckOverlap((uid, comp, Transform(uid)));
-    }
-
-    public bool CheckOverlap(Entity<AnchorableStorageComponent, TransformComponent> ent)
-    {
-        if (ent.Comp2.GridUid is not { } grid || !TryComp<MapGridComponent>(grid, out var gridComp))
+        if (xform.GridUid is not { } grid || !TryComp<MapGridComponent>(grid, out var gridComp))
             return false;
 
-        var indices = _map.TileIndicesFor(grid, gridComp, ent.Comp2.Coordinates);
+        var indices = _map.TileIndicesFor(grid, gridComp, xform.Coordinates);
         var enumerator = _map.GetAnchoredEntitiesEnumerator(grid, gridComp, indices);
 
         while (enumerator.MoveNext(out var otherEnt))
         {
-            // Don't match yourself.
-            if (otherEnt == ent.Owner)
+            // Не сравниваем с самим собой.
+            if (otherEnt == uid)
                 continue;
 
-            // Is another storage entity is already anchored here?
+            // Если другое хранилище уже закреплено здесь.
             if (HasComp<AnchorableStorageComponent>(otherEnt))
                 return true;
         }
