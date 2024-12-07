62c3a722d5525c9fe6e5f6ffd7b52e62c3d1a99b
AwareFoxy <vgstecker@gmail.com>
2024-12-07 18:03:56 +0100

    first commit

---
+++ Content.Server/_CorvaxNext/Ghostbar/Components/GhostBarSpawnComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server._CorvaxNext.Ghostbar.Components;
+
+/// <summary>
+/// Target for ghosts to spawn at
+/// </summary>
+[RegisterComponent]
+public sealed partial class GhostBarSpawnComponent : Component
+{
+
+}
