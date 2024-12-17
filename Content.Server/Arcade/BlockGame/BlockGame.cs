Rxup/space-station-14

[Port] Morale System (#881)
* Morale System

Co-Authored-By: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>
Co-Authored-By: Angelo Fallaria <ba.fallaria@gmail.com>
Co-Authored-By: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>

* Surgery mood effects

* Update mood.ftl

* Create categories.yml

* Add more positive Mood effects & Mood Examine

* Add antag mood effects

---------

Co-authored-by: VMSolidus <evilexecutive@gmail.com>
Co-authored-by: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>
Co-authored-by: Angelo Fallaria <ba.fallaria@gmail.com>
Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>

Content.Server/Arcade/BlockGame/BlockGame.cs
@@ -2,6 +2,7 @@ using Content.Shared.Arcade;
 using Robust.Server.GameObjects;
 using Robust.Shared.Random;
 using System.Linq;
+using Content.Shared.Backmen.Mood;
 
 namespace Content.Server.Arcade.BlockGame;
 
@@ -82,6 +83,8 @@ public sealed partial class BlockGame
         {
             _highScorePlacement = _arcadeSystem.RegisterHighScore(meta.EntityName, Points);
             SendHighscoreUpdate();
+            var ev = new MoodEffectEvent("ArcadePlay"); // backmen: mood
+            _entityManager.EventBus.RaiseLocalEvent(meta.Owner, ev);
         }
         SendMessage(new BlockGameMessages.BlockGameGameOverScreenMessage(Points, _highScorePlacement?.LocalPlacement, _highScorePlacement?.GlobalPlacement));
     }

Content.Server/GameTicking/Rules/NukeopsRuleSystem.cs
@@ -24,6 +24,7 @@ using Robust.Shared.Map;
 using Robust.Shared.Random;
 using Robust.Shared.Utility;
 using System.Linq;
+using Content.Shared.Backmen.Mood;
 using Content.Shared.CombatMode.Pacification;
 using Content.Shared.Store.Components;
 
@@ -493,6 +494,8 @@ public sealed class NukeopsRuleSystem : GameRuleSystem<NukeopsRuleComponent>
         var target = (ent.Comp.TargetStation is not null) ? Name(ent.Comp.TargetStation.Value) : "the target";
 
         RemComp<PacifiedComponent>(args.EntityUid); // Corvax-DionaPacifist: Allow dionas nukes to harm
+        RaiseLocalEvent(args.EntityUid, new MoodEffectEvent("NukeopsFocused")); // backmen: mood
+
         _antag.SendBriefing(args.Session,
             Loc.GetString("nukeops-welcome",
                 ("station", target),

Content.Shared/Backmen/Mood/MoodEffectPrototype.cs
@@ -1,0 +1,35 @@
+﻿using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Backmen.Mood;
+
+[Prototype]
+public sealed class MoodEffectPrototype : IPrototype
+{
+    /// <summary>
+    ///     The ID of the moodlet to use.
+    /// </summary>
+    [IdDataField]
+    public string ID { get; } = default!;
+
+    public string Description => Loc.GetString($"mood-effect-{ID}");
+    /// <summary>
+    ///     If they already have an effect with the same category, the new one will replace the old one.
+    /// </summary>
+    [DataField, ValidatePrototypeId<MoodCategoryPrototype>]
+    public string? Category;
+    /// <summary>
+    ///     How much should this moodlet modify an entity's Mood.
+    /// </summary>
+    [DataField(required: true)]
+    public float MoodChange;
+    /// <summary>
+    ///     How long, in Seconds, does this moodlet last? If omitted, the moodlet will last until canceled by any system.
+    /// </summary>
+    [DataField]
+    public int Timeout;
+    /// <summary>
+    ///     Should this moodlet be hidden from the player? EG: No popups or chat messages.
+    /// </summary>
+    [DataField]
+    public bool Hidden;
+}

Content.Shared/Nutrition/EntitySystems/HungerSystem.cs
@@ -10,6 +10,10 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
 using Robust.Shared.Utility;
+using Content.Shared.Backmen.Mood;
+using Robust.Shared.Network;
+using Robust.Shared.Configuration;
+using Content.Shared.Backmen.CCVar;
 
 namespace Content.Shared.Nutrition.EntitySystems;
 
@@ -23,6 +27,8 @@ public sealed class HungerSystem : EntitySystem
     [Dependency] private readonly MobStateSystem _mobState = default!;
     [Dependency] private readonly MovementSpeedModifierSystem _movementSpeedModifier = default!;
     [Dependency] private readonly SharedJetpackSystem _jetpack = default!;
+    [Dependency] private readonly INetManager _net = default!;
+    [Dependency] private readonly IConfigurationManager _config = default!;
 
     [ValidatePrototypeId<SatiationIconPrototype>]
     private const string HungerIconOverfedId = "HungerIconOverfed";
@@ -58,10 +64,9 @@ public sealed class HungerSystem : EntitySystem
 
     private void OnRefreshMovespeed(EntityUid uid, HungerComponent component, RefreshMovementSpeedModifiersEvent args)
     {
-        if (component.CurrentThreshold > HungerThreshold.Starving)
-            return;
-
-        if (_jetpack.IsUserFlying(uid))
+        if (_config.GetCVar(CCVars.MoodEnabled)
+            || component.CurrentThreshold > HungerThreshold.Starving
+            || _jetpack.IsUserFlying(uid))
             return;
 
         args.ModifySpeed(component.StarvingSlowdownModifier, component.StarvingSlowdownModifier);
@@ -125,7 +130,13 @@ public sealed class HungerSystem : EntitySystem
 
         if (GetMovementThreshold(component.CurrentThreshold) != GetMovementThreshold(component.LastThreshold))
         {
-            _movementSpeedModifier.RefreshMovementSpeedModifiers(uid);
+            if (!_config.GetCVar(CCVars.MoodEnabled))
+                _movementSpeedModifier.RefreshMovementSpeedModifiers(uid);
+            else if (_net.IsServer)
+            {
+                var ev = new MoodEffectEvent("Hunger" + component.CurrentThreshold); // backmen: mood
+                RaiseLocalEvent(uid, ev);
+            }
         }
 
         if (component.HungerThresholdAlerts.TryGetValue(component.CurrentThreshold, out var alertId))

Content.Shared/Nutrition/EntitySystems/SharedCreamPieSystem.cs
@@ -1,6 +1,7 @@
 using Content.Shared.Nutrition.Components;
 using Content.Shared.Stunnable;
 using Content.Shared.Throwing;
+using Content.Shared.Backmen.Mood;
 using JetBrains.Annotations;
 
 namespace Content.Shared.Nutrition.EntitySystems
@@ -44,6 +45,11 @@ namespace Content.Shared.Nutrition.EntitySystems
             {
                 _appearance.SetData(uid, CreamPiedVisuals.Creamed, value, appearance);
             }
+
+            if (value)
+                RaiseLocalEvent(uid, new MoodEffectEvent("Creampied")); // backmen: mood
+            else
+                RaiseLocalEvent(uid, new MoodRemoveEffectEvent("Creampied")); // backmen: mood
         }
 
         private void OnCreamPieLand(EntityUid uid, CreamPieComponent component, ref LandEvent args)

Content.Shared/Slippery/SlipperySystem.cs
@@ -9,6 +9,7 @@ using Content.Shared.StatusEffect;
 using Content.Shared.StepTrigger.Systems;
 using Content.Shared.Stunnable;
 using Content.Shared.Throwing;
+using Content.Shared.Backmen.Mood;
 using JetBrains.Annotations;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
@@ -19,7 +20,7 @@ using Robust.Shared.Utility;
 
 namespace Content.Shared.Slippery;
 
-[UsedImplicitly] 
+[UsedImplicitly]
 public sealed class SlipperySystem : EntitySystem
 {
     [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
@@ -83,7 +84,7 @@ public sealed class SlipperySystem : EntitySystem
     {
         if (HasComp<SpeedModifiedByContactComponent>(args.OtherEntity))
             _speedModifier.AddModifiedEntity(args.OtherEntity);
-    } 
+    }
 
     private bool CanSlip(EntityUid uid, EntityUid toSlip)
     {
@@ -127,6 +128,8 @@ public sealed class SlipperySystem : EntitySystem
         var playSound = !_statusEffects.HasStatusEffect(other, "KnockedDown");
 
         _stun.TryParalyze(other, TimeSpan.FromSeconds(component.ParalyzeTime), true);
+
+        RaiseLocalEvent(other, new MoodEffectEvent("MobSlipped")); // backmen: mood
 
         // Preventing from playing the slip sound when you are already knocked down.
         if (playSound)

space-syndicate/space-station-14-next

Content.Server/Arcade/BlockGame/BlockGame.cs
@@ -2,6 +2,7 @@ using Content.Shared.Arcade;
 using Robust.Server.GameObjects;
 using Robust.Shared.Random;
 using System.Linq;
+using Content.Shared._CorvaxNext.Mood;
 
 namespace Content.Server.Arcade.BlockGame;
 
@@ -82,6 +83,8 @@ public sealed partial class BlockGame
         {
             _highScorePlacement = _arcadeSystem.RegisterHighScore(meta.EntityName, Points);
             SendHighscoreUpdate();
+            var ev = new MoodEffectEvent("ArcadePlay"); // _CorvaxNext: mood
+            _entityManager.EventBus.RaiseLocalEvent(meta.Owner, ev);
         }
         SendMessage(new BlockGameMessages.BlockGameGameOverScreenMessage(Points, _highScorePlacement?.LocalPlacement, _highScorePlacement?.GlobalPlacement));
     }

Content.Server/GameTicking/Rules/NukeopsRuleSystem.cs
@@ -24,6 +24,7 @@ using Robust.Shared.Map;
 using Robust.Shared.Random;
 using Robust.Shared.Utility;
 using System.Linq;
+using Content.Shared._CorvaxNext.Mood;
 using Content.Shared.CombatMode.Pacification;
 using Content.Shared.Store.Components;
 
@@ -478,6 +479,8 @@ public sealed class NukeopsRuleSystem : GameRuleSystem<NukeopsRuleComponent>
         var target = (ent.Comp.TargetStation is not null) ? Name(ent.Comp.TargetStation.Value) : "the target";
 
         RemComp<PacifiedComponent>(args.EntityUid); // Corvax-DionaPacifist: Allow dionas nukes to harm
+        RaiseLocalEvent(args.EntityUid, new MoodEffectEvent("NukeopsFocused")); // _CorvaxNext: mood
+
         _antag.SendBriefing(args.Session,
             Loc.GetString("nukeops-welcome",
                 ("station", target),

Content.Shared/Backmen/Mood/MoodEffectPrototype.cs
@@ -1,0 +1,35 @@
+﻿using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CorvaxNext.Mood;
+
+[Prototype]
+public sealed class MoodEffectPrototype : IPrototype
+{
+    /// <summary>
+    ///     The ID of the moodlet to use.
+    /// </summary>
+    [IdDataField]
+    public string ID { get; } = default!;
+
+    public string Description => Loc.GetString($"mood-effect-{ID}");
+    /// <summary>
+    ///     If they already have an effect with the same category, the new one will replace the old one.
+    /// </summary>
+    [DataField, ValidatePrototypeId<MoodCategoryPrototype>]
+    public string? Category;
+    /// <summary>
+    ///     How much should this moodlet modify an entity's Mood.
+    /// </summary>
+    [DataField(required: true)]
+    public float MoodChange;
+    /// <summary>
+    ///     How long, in Seconds, does this moodlet last? If omitted, the moodlet will last until canceled by any system.
+    /// </summary>
+    [DataField]
+    public int Timeout;
+    /// <summary>
+    ///     Should this moodlet be hidden from the player? EG: No popups or chat messages.
+    /// </summary>
+    [DataField]
+    public bool Hidden;
+}

Content.Shared/Nutrition/EntitySystems/HungerSystem.cs
@@ -10,6 +10,10 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
 using Robust.Shared.Utility;
+using Content.Shared._CorvaxNext.Mood;
+using Robust.Shared.Network;
+using Robust.Shared.Configuration;
+using Content.Shared._CorvaxNext.CCVar;
 
 namespace Content.Shared.Nutrition.EntitySystems;
 
@@ -23,6 +27,8 @@ public sealed class HungerSystem : EntitySystem
     [Dependency] private readonly MobStateSystem _mobState = default!;
     [Dependency] private readonly MovementSpeedModifierSystem _movementSpeedModifier = default!;
     [Dependency] private readonly SharedJetpackSystem _jetpack = default!;
+    [Dependency] private readonly INetManager _net = default!;
+    [Dependency] private readonly IConfigurationManager _config = default!;
 
     [ValidatePrototypeId<SatiationIconPrototype>]
     private const string HungerIconOverfedId = "HungerIconOverfed";
@@ -58,10 +64,9 @@ public sealed class HungerSystem : EntitySystem
 
     private void OnRefreshMovespeed(EntityUid uid, HungerComponent component, RefreshMovementSpeedModifiersEvent args)
     {
-        if (component.CurrentThreshold > HungerThreshold.Starving)
-            return;
-
-        if (_jetpack.IsUserFlying(uid))
+        if (_config.GetCVar(CCVars.MoodEnabled)
+            || component.CurrentThreshold > HungerThreshold.Starving
+            || _jetpack.IsUserFlying(uid))
             return;
 
         args.ModifySpeed(component.StarvingSlowdownModifier, component.StarvingSlowdownModifier);
@@ -125,7 +130,13 @@ public sealed class HungerSystem : EntitySystem
 
         if (GetMovementThreshold(component.CurrentThreshold) != GetMovementThreshold(component.LastThreshold))
         {
-            _movementSpeedModifier.RefreshMovementSpeedModifiers(uid);
+            if (!_config.GetCVar(CCVars.MoodEnabled))
+                _movementSpeedModifier.RefreshMovementSpeedModifiers(uid);
+            else if (_net.IsServer)
+            {
+                var ev = new MoodEffectEvent("Hunger" + component.CurrentThreshold); // _CorvaxNext: mood
+                RaiseLocalEvent(uid, ev);
+            }
         }
 
         if (component.HungerThresholdAlerts.TryGetValue(component.CurrentThreshold, out var alertId))

Content.Shared/Nutrition/EntitySystems/SharedCreamPieSystem.cs
@@ -1,6 +1,7 @@
 using Content.Shared.Nutrition.Components;
 using Content.Shared.Stunnable;
 using Content.Shared.Throwing;
+using Content.Shared._CorvaxNext.Mood;
 using JetBrains.Annotations;
 
 namespace Content.Shared.Nutrition.EntitySystems
@@ -44,6 +45,11 @@ namespace Content.Shared.Nutrition.EntitySystems
             {
                 _appearance.SetData(uid, CreamPiedVisuals.Creamed, value, appearance);
             }
+
+            if (value)
+                RaiseLocalEvent(uid, new MoodEffectEvent("Creampied")); // _CorvaxNext: mood
+            else
+                RaiseLocalEvent(uid, new MoodRemoveEffectEvent("Creampied")); // _CorvaxNext: mood
         }
 
         private void OnCreamPieLand(EntityUid uid, CreamPieComponent component, ref LandEvent args)

Content.Shared/Slippery/SlipperySystem.cs
@@ -9,6 +9,7 @@ using Content.Shared.StatusEffect;
 using Content.Shared.StepTrigger.Systems;
 using Content.Shared.Stunnable;
 using Content.Shared.Throwing;
+using Content.Shared._CorvaxNext.Mood;
 using JetBrains.Annotations;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
@@ -19,7 +20,7 @@ using Robust.Shared.Utility;
 
 namespace Content.Shared.Slippery;
 
-[UsedImplicitly] 
+[UsedImplicitly]
 public sealed class SlipperySystem : EntitySystem
 {
     [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
@@ -83,7 +84,7 @@ public sealed class SlipperySystem : EntitySystem
     {
         if (HasComp<SpeedModifiedByContactComponent>(args.OtherEntity))
             _speedModifier.AddModifiedEntity(args.OtherEntity);
-    } 
+    }
 
     private bool CanSlip(EntityUid uid, EntityUid toSlip)
     {
@@ -127,6 +128,8 @@ public sealed class SlipperySystem : EntitySystem
         var playSound = !_statusEffects.HasStatusEffect(other, "KnockedDown");
 
         _stun.TryParalyze(other, TimeSpan.FromSeconds(component.ParalyzeTime), true);
+
+        RaiseLocalEvent(other, new MoodEffectEvent("MobSlipped")); // _CorvaxNext: mood
 
         // Preventing from playing the slip sound when you are already knocked down.
         if (playSound)