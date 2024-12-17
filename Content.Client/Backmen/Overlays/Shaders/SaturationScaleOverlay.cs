Rxup/space-station-14

[Fix] Mood cool fixes (#916)
* Fix Overlays (#756)

Overlays have a funny bug where the calls to update them are global.
Meaning if any single person gets a bad enough mood to greyscale
themselves, everyone globally gets greyscaled. This bug was also present
on Dogvision and Ultravision, and had the same cause. Frontier luckily
had a fix for those two, and the fix works here as well for the Mood
Overlay.

:cl:
- fix: Fixed an issue where Overlays(Dogvision, Ultravision, Mood) would
apply globally to all entities when updating.

* MoodSystem Crit Threshold CVar (#1010)

This PR adds a CVar that allows server hosts (such as N14 who need it)
to optionally disable the Crit Threshold modification part of the
MoodSystem. This is useful if a server has some other system that
frequently messes with Thresholds, and that a server host wishes their
system to not have to fight with Mood for it.

No changelog because this isn't playerfacing.

* Update SaturationScaleOverlay.cs

---------

Co-authored-by: VMSolidus <evilexecutive@gmail.com>

Content.Client/Backmen/Overlays/Shaders/SaturationScaleOverlay.cs
@@ -1,4 +1,7 @@
-﻿using Robust.Client.Graphics;
+﻿using System.Numerics;
+using Content.Shared.Backmen.Overlays;
+using Robust.Client.Graphics;
+using Robust.Client.Player;
 using Robust.Shared.Enums;
 using Robust.Shared.Prototypes;
 
@@ -7,6 +10,8 @@ namespace Content.Client.Backmen.Overlays.Shaders;
 public sealed class SaturationScaleOverlay : Overlay
 {
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] IEntityManager _entityManager = default!;
 
     public override bool RequestScreenTexture => true;
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
@@ -18,20 +23,29 @@ public sealed class SaturationScaleOverlay : Overlay
     {
         IoCManager.InjectDependencies(this);
 
-        _shader = _prototypeManager.Index<ShaderPrototype>("SaturationScale").InstanceUnique();
+        _shader = _prototypeManager.Index<ShaderPrototype>("SaturationScale").Instance().Duplicate();
+    }
+
+    protected override bool BeforeDraw(in OverlayDrawArgs args)
+    {
+        if (_playerManager.LocalEntity is not { Valid: true } player
+            || !_entityManager.HasComponent<SaturationScaleOverlayComponent>(player))
+            return false;
+
+        return base.BeforeDraw(in args);
     }
 
 
     protected override void Draw(in OverlayDrawArgs args)
     {
-        if (ScreenTexture == null)
+        if (ScreenTexture is null)
             return;
 
         _shader.SetParameter("SCREEN_TEXTURE", ScreenTexture);
         _shader.SetParameter("saturation", Saturation);
 
         var handle = args.WorldHandle;
-
+        handle.SetTransform(Matrix3x2.Identity);
         handle.UseShader(_shader);
         handle.DrawRect(args.WorldBounds, Color.White);
         handle.UseShader(null);

space-syndicate/space-station-14-next

Content.Client/Backmen/Overlays/Shaders/SaturationScaleOverlay.cs
@@ -1,4 +1,7 @@
-﻿using Robust.Client.Graphics;
+﻿using System.Numerics;
+using Content.Shared._CorvaxNext.Overlays;
+using Robust.Client.Graphics;
+using Robust.Client.Player;
 using Robust.Shared.Enums;
 using Robust.Shared.Prototypes;
 
@@ -7,6 +10,8 @@ namespace Content.Client._CorvaxNext.Overlays.Shaders;
 public sealed class SaturationScaleOverlay : Overlay
 {
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] IEntityManager _entityManager = default!;
 
     public override bool RequestScreenTexture => true;
     public override OverlaySpace Space => OverlaySpace.WorldSpace;
@@ -18,20 +23,29 @@ public sealed class SaturationScaleOverlay : Overlay
     {
         IoCManager.InjectDependencies(this);
 
-        _shader = _prototypeManager.Index<ShaderPrototype>("SaturationScale").InstanceUnique();
+        _shader = _prototypeManager.Index<ShaderPrototype>("SaturationScale").Instance().Duplicate();
+    }
+
+    protected override bool BeforeDraw(in OverlayDrawArgs args)
+    {
+        if (_playerManager.LocalEntity is not { Valid: true } player
+            || !_entityManager.HasComponent<SaturationScaleOverlayComponent>(player))
+            return false;
+
+        return base.BeforeDraw(in args);
     }
 
 
     protected override void Draw(in OverlayDrawArgs args)
     {
-        if (ScreenTexture == null)
+        if (ScreenTexture is null)
             return;
 
         _shader.SetParameter("SCREEN_TEXTURE", ScreenTexture);
         _shader.SetParameter("saturation", Saturation);
 
         var handle = args.WorldHandle;
-
+        handle.SetTransform(Matrix3x2.Identity);
         handle.UseShader(_shader);
         handle.DrawRect(args.WorldBounds, Color.White);
         handle.UseShader(null);