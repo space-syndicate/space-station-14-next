4ac46eed2bd58957f991eaa13a3943a2c4db6704
Vonsant <von7ant@gmail.com>
2024-12-23 22:02:45 +0300

    SecCam

---
+++ Content.Server/_CorvaxNext/BodyCam/BodyCameraComponent.cs
@@ -0,0 +1,12 @@
+using Robust.Shared.GameObjects;
+
+namespace Content.Server.Camera
+{
+    /// <summary>
+    /// A marker component for cameras that should only be active when worn.
+    /// </summary>
+    [RegisterComponent]
+    public sealed partial class BodyCameraComponent : Component
+    {
+    }
+}
