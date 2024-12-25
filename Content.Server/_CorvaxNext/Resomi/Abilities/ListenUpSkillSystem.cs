772b36146f1ad757fd0619508729843325a6db6e
pofitlo <kuzminvladislav237@gmail.com>
2024-12-25 13:51:00 +0400

    second

--- Content.Server/_CorvaxNext/Resomi/Abilities/ListenUpSkillSystem.cs
+++ Content.Server/_CorvaxNext/Resomi/Abilities/ListenUpSkillSystem.cs
@@ -1,15 +1,3 @@
-using Content.Shared.Actions;
-using Content.Shared.Alert;
-using Content.Shared.Maps;
-using Robust.Shared.Containers;
-using Robust.Shared.Map;
-using Robust.Shared.Timing;
-using Content.Shared._CorvaxNext.Resomi;
-using Content.Shared.Movement.Components;
-using Content.Shared.Movement.Systems;
-using Content.Shared._CorvaxNext.Resomi.Abilities;
-using Content.Shared.Damage.Components;
-using Robust.Shared.Physics;
 using Content.Shared._CorvaxNext.Resomi.Abilities.Hearing;
 
 namespace Content.Server._CorvaxNext.Resomi.Abilities;
@@ -20,11 +8,10 @@ public sealed class ListenUpSkillSystem : SharedListenUpSkillSystem
     {
         base.Initialize();
         SubscribeLocalEvent<ListenUpSkillComponent, ComponentInit>(OnComponentInit);
-        SubscribeLocalEvent<ListenUpSkillComponent, ListenUpActionEvent>(SwitchAgility);
-        SubscribeLocalEvent<ListenUpSkillComponent, RefreshMovementSpeedModifiersEvent>(OnRefreshMovespeed);
     }
     private void OnComponentInit(Entity<ListenUpSkillComponent> ent, ref ComponentInit args)
     {
-        _actionsSystem.AddAction(ent.Owner, ref ent.Comp.SwitchAgilityActionEntity, ent.Comp.SwitchAgilityAction, ent.Owner);
+        _actionsSystem.AddAction(ent.Owner, ref ent.Comp.SwitchListenUpActionEntity, ent.Comp.SwitchListenUpAction, ent.Owner);
+
     }
 }
--- Content.Shared/_CorvaxNext/Resomi/Abilities/Hearing/ListenUpSkillComponent.cs
+++ Content.Shared/_CorvaxNext/Resomi/Abilities/Hearing/ListenUpSkillComponent.cs
@@ -4,9 +4,19 @@ using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototy
 
 namespace Content.Shared._CorvaxNext.Resomi.Abilities.Hearing;
 
-[RegisterComponent, NetworkedComponent]
-[AutoGenerateComponentState]
+[RegisterComponent]
 public sealed partial class ListenUpSkillComponent : Component
 {
 
+    [DataField("switchListenUpAction", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string? SwitchListenUpAction = "SwitchListenUpAction";
+
+    [DataField]
+    public EntityUid? SwitchListenUpActionEntity;
+
+    [DataField]
+    public bool toggled = false;
+
+    [DataField]
+    public float prepareTime = 3f;
 }
