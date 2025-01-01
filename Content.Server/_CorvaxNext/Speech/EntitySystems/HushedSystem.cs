1bac56226cf5244d76a2988e13fde5e70dade0aa
Kill_Me_I_Noobs <118206719+Vonsant@users.noreply.github.com>
2025-01-01 14:41:17 +0300

    Update HushedSystem.cs

--- Content.Server/_CorvaxNext/Speech/EntitySystems/HushedSystem.cs
+++ Content.Server/_CorvaxNext/Speech/EntitySystems/HushedSystem.cs
@@ -17,8 +17,8 @@ public sealed class HushedSystem : EntitySystem
 
     public override void Initialize()
     {
-        SubscribeLocalEvent<HushedComponent, ScreamActionEvent>(OnScreamAction, new[] { typeof(VocalSystem) });
-        SubscribeLocalEvent<HushedComponent, EmoteEvent>(OnEmote, before: new[] { typeof(VocalSystem) });
+        SubscribeLocalEvent<HushedComponent, ScreamActionEvent>(OnScreamAction, [typeof(VocalSystem)]);
+        SubscribeLocalEvent<HushedComponent, EmoteEvent>(OnEmote, before: [typeof(VocalSystem)]);
     }
 
     private void OnScreamAction(EntityUid uid, HushedComponent component, ScreamActionEvent args)
