154d0eadf2fe48911011b2f10fbc871a50c57ba8
Vonsant <von7ant@gmail.com>
2025-01-02 08:04:25 +0300

    Medals

---
+++ Content.Shared/_CorvaxNext/CrewMedal/CrewMedalEvents.cs
@@ -0,0 +1,29 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._CorvaxNext.CrewMedal;
+
+/// <summary>
+/// Enum representing the key for the Crew Medal user interface.
+/// </summary>
+[Serializable, NetSerializable]
+public enum CrewMedalUiKey : byte
+{
+    Key
+}
+
+/// <summary>
+/// Message sent when the reason for the medal is changed via the user interface.
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class CrewMedalReasonChangedMessage : BoundUserInterfaceMessage
+{
+    /// <summary>
+    /// The new reason for the medal.
+    /// </summary>
+    public string Reason { get; }
+
+    public CrewMedalReasonChangedMessage(string reason)
+    {
+        Reason = reason;
+    }
+}
