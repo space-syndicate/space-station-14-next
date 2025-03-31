cd5f034cc85bac46560c7ebb769b7a21fecee4ef
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2025-03-30 23:06:03 -1000

    Movement Impaired!
    Signed-off-by: SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
    
    # Conflicts:
    #	Content.Goobstation.Shared/Traits/Assorted/LegsParalyzedSystem.cs
    #	Content.Goobstation.Shared/Traits/Systems/LegsParalyzedSystem.cs
    #	Content.Goobstation.Shared/Traits/Systems/LegsStartParalyzedSystem.cs
    #	Content.Shared/Traits/Assorted/LegsParalyzedSystem.cs
    #	Resources/Locale/en-US/_Goobstation/traits/traits.ftl
    #	Resources/Prototypes/Entities/Objects/Weapons/Melee/cane.yml
    #	Resources/Prototypes/_Goobstation/Recipes/Lathes/Packs/medical.yml
    #	Resources/Prototypes/_Goobstation/Recipes/Lathes/medical.yml
    #	Resources/Prototypes/_Goobstation/Traits/disabilities.yml

---
+++ Content.Shared/_Goobstation/Traits/Components/MovementImpairedComponent.cs
@@ -0,0 +1,25 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._GoobStation.Traits.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class MovementImpairedComponent : Component
+{
+    /// <summary>
+    /// What number is this entities speed multiplied by when impaired?
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public float ImpairedSpeedMultiplier { get; set; } = 0.4f;
+
+    /// <summary>
+    /// The original speed multiplier of the entity, stored and restored when the item is picked up or put down.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public float BaseImpairedSpeedMultiplier { get; set; } = 0.4f;
+
+    /// <summary>
+    /// How many movement correcting items the entity has.
+    /// </summary>
+    [DataField]
+    public int CorrectionCounter { get; set; } = 0;
+}
---
+++ Content.Shared/_Goobstation/Traits/Components/MovementImpairedCorrectionComponent.cs
@@ -0,0 +1,14 @@
+namespace Content.Shared._GoobStation.Traits.Components;
+
+[RegisterComponent]
+public sealed partial class MovementImpairedCorrectionComponent : Component
+{
+    /// <summary>
+    /// How much should the impaired speed be fixed by this component?
+    /// </summary>
+    /// <remarks>
+    /// If set to zero, remove the impaired speed entirely.
+    /// </remarks>
+    [DataField]
+    public float SpeedCorrection = 0f;
+}
