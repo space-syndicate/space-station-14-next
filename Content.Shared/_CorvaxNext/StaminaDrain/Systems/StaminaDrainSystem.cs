af496b41425ea1f63867d805b728e1c3c84fca70
61ya <6161yaya@gmail.com>
2025-05-14 01:26:46 +0300

    fix

--- Content.Shared/_CorvaxNext/StaminaDrain/Systems/StaminaDrainSystem.cs
+++ Content.Shared/_CorvaxNext/StaminaDrain/Systems/StaminaDrainSystem.cs
@@ -5,8 +5,6 @@ using Content.Shared.Inventory;
 using Content.Shared.Item.ItemToggle;
 using Content.Shared.Item.ItemToggle.Components;
 using Robust.Shared.Containers;
-using Robust.Shared.Player;
-using Robust.Shared.Localization;
 using Content.Shared.Popups;
 
 namespace Content.Shared.Clothing.EntitySystems;
@@ -16,10 +14,9 @@ public sealed class StaminaDrainSystem : EntitySystem
     [Dependency] private readonly ItemToggleSystem _toggle = default!;
     [Dependency] private readonly StaminaSystem _stamina = default!;
     [Dependency] private readonly SharedContainerSystem _container = default!;
-    [Dependency] private readonly ISharedPlayerManager _playerManager = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
 
-    private readonly HashSet<EntityUid> _notified = new();
+    private readonly HashSet<EntityUid> _notifiedWearers = new();
 
     public override void Update(float frameTime)
     {
@@ -31,7 +28,7 @@ public sealed class StaminaDrainSystem : EntitySystem
             if (!toggle.Activated)
             {
                 if (_container.TryGetContainingContainer(uid, out var containerTmp))
-                    _notified.Remove(containerTmp.Owner);
+                    _notifiedWearers.Remove(containerTmp.Owner);
                 continue;
             }
 
@@ -42,11 +39,8 @@ public sealed class StaminaDrainSystem : EntitySystem
             if (!EntityManager.HasComponent<StaminaComponent>(wearer))
                 continue;
 
-            if (!_notified.Contains(wearer))
-            {
-                _notified.Add(wearer);
+            if (_notifiedWearers.Add(wearer))
                 _popup.PopupClient(Loc.GetString("stamina-drain-popup"), wearer);
-            }
 
             _stamina.TakeStaminaDamage(wearer, drain.StaminaPerSecond * frameTime, visual: false);
         }
