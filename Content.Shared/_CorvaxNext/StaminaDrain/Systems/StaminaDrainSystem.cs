d6238f1ed1c44d81f8c48f967598985e285b11eb
61ya <6161yaya@gmail.com>
2025-05-19 15:18:11 +0300

    update-fix

--- Content.Shared/_CorvaxNext/StaminaDrain/Systems/StaminaDrainSystem.cs
+++ Content.Shared/_CorvaxNext/StaminaDrain/Systems/StaminaDrainSystem.cs
@@ -7,6 +7,7 @@ using Content.Shared.Item.ItemToggle;
 using Content.Shared.Item.ItemToggle.Components;
 using Robust.Shared.Containers;
 using Content.Shared.Popups;
+using Robust.Shared.Timing;
 
 namespace Content.Shared._CorvaxNext.StaminaDrain.Systems;
 
@@ -16,12 +17,20 @@ public sealed class StaminaDrainSystem : EntitySystem
     [Dependency] private readonly StaminaSystem _stamina = default!;
     [Dependency] private readonly SharedContainerSystem _container = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
 
     private readonly HashSet<EntityUid> _notifiedWearers = new();
+    private TimeSpan _nextUpdateTime = TimeSpan.Zero;
+    private static readonly TimeSpan UpdateInterval = TimeSpan.FromSeconds(0.1);
 
     public override void Update(float frameTime)
     {
         base.Update(frameTime);
+
+        if (_timing.CurTime < _nextUpdateTime)
+            return;
+
+        _nextUpdateTime = _timing.CurTime + UpdateInterval;
 
         var query = EntityQueryEnumerator<StaminaDrainComponent, ItemToggleComponent>();
         while (query.MoveNext(out var uid, out var drain, out var toggle))
@@ -43,7 +52,7 @@ public sealed class StaminaDrainSystem : EntitySystem
             if (_notifiedWearers.Add(wearer))
                 _popup.PopupClient(Loc.GetString("stamina-drain-popup"), wearer);
 
-            _stamina.TakeStaminaDamage(wearer, drain.StaminaPerSecond * frameTime, visual: false);
+            _stamina.TakeStaminaDamage(wearer, drain.StaminaPerSecond * (float)UpdateInterval.TotalSeconds, visual: false);
         }
     }
 }
