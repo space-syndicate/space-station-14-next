ea4bc5e7ec9537b3f7b847452b773bdec9cfd3f6
Artem <dorokhovartem2009@gmail.com>
2025-05-23 18:42:54 +0800

    yep

--- Content.Shared/Roles/JobPrototype.cs
+++ Content.Shared/Roles/JobPrototype.cs
@@ -50,6 +50,9 @@ namespace Content.Shared.Roles
         [DataField, Access(typeof(SharedRoleSystem), Other = AccessPermissions.None)]
         public HashSet<JobRequirement>? Requirements;
 
+        [DataField, Access(typeof(SharedRoleSystem), Other = AccessPermissions.None)] // CorvaxNext(Frontier)
+        public Dictionary<string, HashSet<JobRequirement>>? AlternateRequirementSets; // CorvaxNext(Frontier): sets of requirements - one must be matched in order to
+
         /// <summary>
         ///     When true - the station will have anouncement about arrival of this player.
         /// </summary>
--- Content.Shared/Roles/SharedRoleSystem.cs
+++ Content.Shared/Roles/SharedRoleSystem.cs
@@ -630,6 +630,19 @@ public abstract class SharedRoleSystem : EntitySystem
 
         return antag.Requirements;
     }
+
+    // CorvaxNext(Frontier): alternate requirement sets
+    public Dictionary<string, HashSet<JobRequirement>>? GetAlternateJobRequirements(JobPrototype job)
+    {
+        return job.AlternateRequirementSets;
+    }
+
+    public Dictionary<string, HashSet<JobRequirement>>? GetAlternateJobRequirements(ProtoId<JobPrototype> job)
+    {
+        return _prototypes.Index(job).AlternateRequirementSets;
+    }
+    // End CorvaxNext(Frontier): alternate requirement sets
+
 }
 
 /// <summary>
