5f0bb43b47311e480cacda8670644f16f4af774b
Artem <dorokhovartem2009@gmail.com>
2025-05-30 13:58:34 +0800

    fix

--- Content.Shared.Database/LogType.cs
+++ Content.Shared.Database/LogType.cs
@@ -464,4 +464,9 @@ public enum LogType
     /// Logs related to botany, such as planting and harvesting crops
     /// </summary>
     Botany = 100,
+
+    /// <summary>
+    /// A player used RPD.
+    /// </summary>
+    RPD = 101,
 }
