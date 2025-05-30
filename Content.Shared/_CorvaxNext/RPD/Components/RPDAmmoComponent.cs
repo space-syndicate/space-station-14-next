7589fd6f8b597e9d9d61c2edd9c8a9370e413808
Artem <dorokhovartem2009@gmail.com>
2025-05-30 13:05:10 +0800

    commit for future

--- Content.Shared/_CorvaxNext/RPD/Components/RPDAmmoComponent.cs
+++ Content.Shared/_CorvaxNext/RPD/Components/RPDAmmoComponent.cs
@@ -16,12 +16,12 @@ public sealed partial class RPDAmmoComponent : Component
     public FixedPoint2 Charges = 50;
 }
 
-public sealed partial class AdvRPDAmmoComponent : Component
-{
-    /// <summary>
-    /// How many charges are contained in this ammo cartridge.
-    /// Can be partially transferred into an RCD, until it is empty then it gets deleted.
-    /// </summary>
-    [DataField("charges"), ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
-    public FixedPoint2 Charges = 250;
-}
+// public sealed partial class AdvRPDAmmoComponent : Component
+// {
+//     /// <summary>
+//     /// How many charges are contained in this ammo cartridge.
+//     /// Can be partially transferred into an RCD, until it is empty then it gets deleted.
+//     /// </summary>
+//     [DataField("charges"), ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+//     public FixedPoint2 Charges = 250;
+// }
